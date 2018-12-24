a465a956cb1c2fd694a51f7d67b2f36a19e2951b
Charlie <charles.mulic@gmail.com>
2018-12-24 11:55:33 -0600

    initial project

---
+++ src/main/java/com/charliemulic/sg/restfulserviceactuator/RestfulServiceActuatorApplication.java
@@ -0,0 +1,14 @@
+package com.charliemulic.sg.restfulserviceactuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServiceActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulServiceActuatorApplication.class, args);
+    }
+
+}
+
